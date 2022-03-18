b40122038eb254ecddf77c20868259866d417a74
zeyuli <li_zeyu@pku.edu.cn>
2022-03-18 16:09:57 +0800

    o3->ofast add openmp for tenmat etc

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
-#if [ -d "./build" ]; then
-#    rm -rf build
-#fi
-#mkdir build
+if [ -d "./build" ]; then
+    rm -rf build
+fi
+mkdir build
 
 cd build || exit
 cmake -DUSE_OPENMP=1 -DUSE_LAPACK=1 -DUSE_MPI=1 ..
